5eebd8931078483002a94efc5b7f278df8c36c9d
feliciebernard <felicie.bernard@outlook.fr>
2022-10-21 09:25:37 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { IWilder } from "./interfaces";
+import Wilder from "./components/Wilder";
+import AddWilderForm from "./components/AddWilderForm";
+import AddSkillsToWilder from "./components/AddSkillsToWilder";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+} from "@apollo/client";
+import { getWilders } from "./graphql/wilders";
+
+function Main() {
+  const { loading, data, refetch } = useQuery<{ wilders: IWilder[] }>(
+    getWilders
+  );
+
+  const [selectedWilder, setSelectedWilder] = useState<IWilder | null>(null);
+
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <h1>Wilders Book</h1>
+        </div>
+      </header>
+      <main className="container">
+        <h2>Wilders</h2>
+        <section className="card-row">
+          {loading === true && "Chargement..."}
+          {data?.wilders.map((wilder) => {
+            return (
+              <Wilder
+                key={wilder.id}
+                id={wilder.id}
+                name={wilder.name}
+                city={wilder.city}
+                upvotes={wilder.upvotes}
+                onAddSkillsClicked={function () {
+                  setSelectedWilder(wilder);
+                }}
+              />
+            );
+          })}
+        </section>
+        {selectedWilder && (
+          <AddSkillsToWilder
+            wilder={selectedWilder}
+            onCancelClicked={() => setSelectedWilder(null)}
+            onWilderUpdated={() => refetch()}
+          />
+        )}
+        <AddWilderForm onWilderCreated={() => refetch()} />
+      </main>
+      <footer>
+        <div className="container">
+          <p>&copy; 2022 Wild Code School</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
   );
 }
 
---
+++ src/components/Skill.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const Circle = styled.span<{ upvotes: number; withMargins?: boolean }>`
+  /* Center the content */
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  /* Colors */
+  background-color: ${(props) => (props.upvotes > 20 ? "green" : "red")};
+  color: #fff;
+  /* Rounded border */
+  border-radius: 9999px;
+  height: 20px;
+  width: 20px;
+  margin-left: ${(props) => (props.withMargins ? "8px" : "0px")};
+`;
+
+interface IProps {
+  name: string;
+  upvotes: number;
+}
+
+function Skill(props: IProps): JSX.Element {
+  return (
+    <li>
+      {props.name}
+      <Circle upvotes={props.upvotes} withMargins={true}>
+        {props.upvotes}
+      </Circle>
+    </li>
+  );
+}
+
+export default Skill;
